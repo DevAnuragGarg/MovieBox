3b9a4c72c4cdf1b5fc00f9bd539534ee8b524d53
Anurag Garg <anurag.garg@u2opiamobile.com>
2019-04-22 12:22:53 +0530

    - Again some changes for CI

--- app/src/main/java/com/intact/filmireview/data/BaseDataManager.kt
+++ app/src/main/java/com/intact/filmireview/data/BaseDataManager.kt
@@ -3,6 +3,11 @@ package com.intact.filmireview.data
 import com.intact.filmireview.data.response.PopularMovieResponse
 import io.reactivex.Single
 
+/**
+ * Base data manager interface having all the functions listed to fetch the
+ * data from server or database
+ */
+
 interface BaseDataManager {
 
     fun getPopularMovies(pageNumber: String) : Single<PopularMovieResponse>
--- app/src/main/java/com/intact/filmireview/data/DataManager.kt
+++ app/src/main/java/com/intact/filmireview/data/DataManager.kt
@@ -5,7 +5,7 @@ import com.intact.filmireview.util.MOVIE_DB_API_KEY
 import javax.inject.Inject
 
 /**
- * this class helps in providing the data from the server
+ *  implementation of base data manager
  */
 
 class DataManager @Inject constructor(private val homeRequests: HomeRequests) : BaseDataManager {
